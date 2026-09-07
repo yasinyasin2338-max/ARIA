e0d4cd7e61e10cada95077469b782040b77900aa
Yasin <yasinyasin2338@gmail.com>
2026-09-07 22:00:27 +0330

    Remove obsolete root Android service

--- AriaSessionService.kt
+++
@@ -1,4 +0,0 @@
-package com.aria
-import android.service.voice.VoiceInteractionSession
-import android.service.voice.VoiceInteractionSessionService
-class AriaSessionService: VoiceInteractionSessionService(){override fun onNewSession(args: android.os.Bundle?): VoiceInteractionSession = VoiceInteractionSession(this)}
