527565e678b0503a509814a60e662560d467cbdd
Yasin <yasinyasin2338@gmail.com>
2026-09-07 22:00:34 +0330

    Remove obsolete root Android service

--- AriaVoiceInteractionService.kt
+++
@@ -1,7 +0,0 @@
-package com.aria
-import android.content.Intent
-import android.service.voice.VoiceInteractionService
-class AriaVoiceInteractionService: VoiceInteractionService(){
- override fun onReady(){super.onReady()}
- override fun onLaunchVoiceAssistFromKeyguard(){startActivity(Intent(this,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP))}
-}
